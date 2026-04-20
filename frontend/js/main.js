7404992180024167d8928c67bb1d085960a88329
CoteMiguel <jmartinez96@outlook.cl>
2026-04-20 13:00:26 -0400

    feat: estructura inicial de prueba tecnica + conexion a sqlserver

---
+++ frontend/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("loginForm").addEventListener("submit", function(e) {
+        e.preventDefault();
+        alert("Pre-Acceso");
+    });
+
+});
